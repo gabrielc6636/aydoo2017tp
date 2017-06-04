08cbeae28c496f6a5f9c43ae356de4fb75bab9b5
rocioechegaraysierra <rocio.echegaray.sierra@gmail.com>
2017-06-04 19:48:59 -0300

    Se implementa POST

--- ejercicio-calendario/app.rb
+++ ejercicio-calendario/app.rb
@@ -4,7 +4,12 @@ require_relative 'model/gestorcalendario'
 gestor = GestorCalendario.new
 
 get  '/calendarios'  do
-		calendarios = gestor.calendarios
-    "#{calendarios}"
+	calendarios = gestor.calendarios
+    	"#{calendarios}"
+end
+
+post  '/calendarios'  do
+	calendario = Calendario.new (params['nombre'])
+	gestor.agregarCalendario(calendario)
 end
 
