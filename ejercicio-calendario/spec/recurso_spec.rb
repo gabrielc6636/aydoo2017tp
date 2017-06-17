9bb3c45ad14f175651e73459e57e30c1a5a4fa94
davidperezramirez <pucarauntref@gmail.com>
2017-06-17 17:24:24 -0300

    Recursos: test de reserva de recurso

--- ejercicio-calendario/spec/recurso_spec.rb
+++ ejercicio-calendario/spec/recurso_spec.rb
@@ -5,8 +5,14 @@ describe 'Recurso' do
 
 	let (:recurso) {Recurso.new("id_recurso")}
 
-	it "recurso recien al preguntarle si esta esta siendo usado deberia devolver false" do
+	it "recurso recien creado deberia esta disponible para su uso" do
 		expect(recurso.estaEnUso?).to eq false
+	end
+
+	it "Reservar el recurso deberia dejarlo en estado en uso " do
+		recurso.reservarRecurso
+
+		expect(recurso.estaEnUso?).to eq true
 	end
 
 end
