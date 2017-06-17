d7df891196e175db72a8e42826a7406c38cbd044
davidperezramirez <pucarauntref@gmail.com>
2017-06-17 20:01:10 -0300

    nombre de recurso unico

--- ejercicio-calendario/model/controladorRecursos.rb
+++ ejercicio-calendario/model/controladorRecursos.rb
@@ -15,8 +15,8 @@ class ControladorRecursos
 	end
 
 	def agregarRecurso(nombreRecurso)
-		validadorDeRecurso.validarRecursoExistente(nombreRecurso, repositorioRecursos)
-	    recurso = Recurso.new(nombreRecurso)
+		validadorDeRecurso.validarRecursoExistente(nombreRecurso.downcase, repositorioRecursos)
+	    recurso = Recurso.new(nombreRecurso.downcase)
 
 	    repositorioRecursos.agregarRecurso(recurso)
 	    salida = FormateadorJson.formatear_coleccion(repositorioRecursos.obtenerRecursos())
@@ -25,7 +25,7 @@ class ControladorRecursos
 
 	def cargarRecursos(listaRecursos)
 		listaRecursos.each do |jsonRecurso|
-			recurso = Recurso.new(jsonRecurso['nombre'])
+			recurso = Recurso.new(jsonRecurso['nombre'].downcase)
 			recurso.enUso = jsonRecurso['enUso']
 			repositorioRecursos.agregarRecurso(recurso)
 		end
@@ -37,12 +37,12 @@ class ControladorRecursos
 	end
 
 	def obtenerRecurso(id_recurso)
-		repositorioRecursos.obtenerRecurso(id_recurso)
+		repositorioRecursos.obtenerRecurso(id_recurso.downcase)
 	end
 
 	def eliminarRecurso(id_recurso)
-		validadorDeRecurso.validarRecursoInExistente(id_recurso, repositorioRecursos)
-		recurso = repositorioRecursos.obtenerRecurso(id_recurso)
+		validadorDeRecurso.validarRecursoInExistente(id_recurso.downcase, repositorioRecursos)
+		recurso = repositorioRecursos.obtenerRecurso(id_recurso.downcase)
 
 		repositorioRecursos.eliminarRecurso(recurso)
 		recursos = obtenerRecursos()
@@ -51,11 +51,11 @@ class ControladorRecursos
 	end
 
 	def asignarRecursoAEvento(id_recurso, id_evento)
-		validadorDeRecurso.validarRecursoInExistente(id_recurso, repositorioRecursos)
-		validadorDeRecurso.validarRecursoSinUso(id_recurso, repositorioRecursos)
-		recurso = repositorioRecursos.obtenerRecurso(id_recurso)		
+		validadorDeRecurso.validarRecursoInExistente(id_recurso.downcase, repositorioRecursos)
+		validadorDeRecurso.validarRecursoSinUso(id_recurso.downcase, repositorioRecursos)
+		recurso = repositorioRecursos.obtenerRecurso(id_recurso.downcase)		
 		
-		evento = Evento.eventos.fetch(id_evento)
+		evento = Evento.eventos.fetch(id_evento.downcase)
 		if evento.nil?
 			raise NameError.new("El evento es inexistente")
 		end
