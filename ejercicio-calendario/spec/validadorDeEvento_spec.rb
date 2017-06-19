a84c1f7a80e16c1ce1b8af2b98ee712b73e67212
davidperezramirez <pucarauntref@gmail.com>
2017-06-18 22:22:37 -0300

    test validarFinalizado

--- ejercicio-calendario/spec/validadorDeEvento_spec.rb
+++ ejercicio-calendario/spec/validadorDeEvento_spec.rb
@@ -6,12 +6,35 @@ describe 'validadorDeEvento' do
 
   let (:validador) {ValidadorDeEvento.new}
   let (:calendario) {calendario = Calendario.new("Un calendario")}
-  let (:evento) {Evento.new(calendario,"id_evento", "nombre_evento", "2017-03-31T18:00:00-03:00", "2017-03-31T20:00:00-03:00")}  
+  let (:evento) {Evento.new(calendario,"id_evento", "nombre_evento", "2017-03-31T18:00:00-03:00", "2017-03-31T20:00:00-03:00")}    
 
-  it "validar evento finalzado para evento no finalizado deberia devolver excepcion" do
+  before(:each) {
+  	Evento.class_variable_set :@@eventos, {}
+  	Calendario.class_variable_set :@@calendarios, {}
+  }
+
+  it "validarEventoSinTerminar para evento no finalizado deberia devolver excepcion" do
 	evento.stub(:estaFinalizado?) { false }
 
-    expect{validador.validarEventoSinTerminar(evento)}.to raise_error(NameError)
+    expect{validador.validarEventoSinTerminar(evento)}.to raise_error(StandardError)
   end  
+
+  it "validarEventoSinTerminar para evento finalizado no deberia devolver excepcion" do
+	evento.stub(:estaFinalizado?) { true }
+
+    expect{validador.validarEventoSinTerminar(evento)}.not_to raise_error
+  end
+
+  it "validarEventoFinalizado para evento finalizado deberia devolver excepcion" do
+	evento.stub(:estaFinalizado?) { true }
+
+    expect{validador.validarEventoFinalizado(evento)}.to raise_error
+  end
+
+  it "validarEventoFinalizado para evento no finalizado no deberia devolver excepcion" do
+	evento.stub(:estaFinalizado?) { false }
+
+    expect{validador.validarEventoFinalizado(evento)}.not_to raise_error
+  end
 
 end
